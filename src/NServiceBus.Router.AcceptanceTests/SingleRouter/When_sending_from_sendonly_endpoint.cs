3bf08db48861b4fc492ab6ecb2fd35b341fe4aa6
SzymonPobiega <szymon.pobiega@gmail.com>
2019-01-11 13:26:04 +0100

    Failing test for send-only endpoint

---
+++ src/NServiceBus.Router.AcceptanceTests/SingleRouter/When_sending_from_sendonly_endpoint.cs
@@ -0,0 +1,88 @@
+﻿using System.Threading.Tasks;
+using NServiceBus.AcceptanceTesting;
+using NServiceBus.AcceptanceTests;
+using NServiceBus.AcceptanceTests.EndpointTemplates;
+using NUnit.Framework;
+
+namespace NServiceBus.Router.AcceptanceTests.SingleRouter
+{
+    using AcceptanceTesting.Customization;
+
+    [TestFixture]
+    public class When_sending_from_sendonly_endpoint : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_deliver_the_message()
+        {
+            var result = await Scenario.Define<Context>()
+                .WithRouter("Router", cfg =>
+                {
+                    cfg.AddInterface<TestTransport>("Left", t => t.BrokerAlpha()).InMemorySubscriptions();
+                    cfg.AddInterface<TestTransport>("Right", t => t.BrokerBravo()).InMemorySubscriptions();
+
+                    cfg.UseStaticRoutingProtocol().AddForwardRoute("Left", "Right");
+                })
+                .WithEndpoint<Sender>(c => c.When(s => s.Send(new MyMessage())))
+                .WithEndpoint<Receiver>()
+                .Done(c => c.MessageReceived)
+                .Run();
+
+            Assert.IsTrue(result.MessageReceived);
+        }
+
+        class Context : ScenarioContext
+        {
+            public bool MessageReceived { get; set; }
+        }
+
+        class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.SendOnly();
+                    var routing = c.UseTransport<TestTransport>().BrokerAlpha().Routing();
+                    var bridge = routing.ConnectToRouter("Router");
+
+                    bridge.RouteToEndpoint(typeof(MyMessage), Conventions.EndpointNamingConvention(typeof(Receiver)));
+                });
+            }
+        }
+
+        class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.UseTransport<TestTransport>().BrokerBravo();
+                });
+            }
+
+            class MyMessageHandler : IHandleMessages<MyMessage>
+            {
+                Context scenarioContext;
+
+                public MyMessageHandler(Context scenarioContext)
+                {
+                    this.scenarioContext = scenarioContext;
+                }
+
+                public Task Handle(MyMessage message, IMessageHandlerContext context)
+                {
+                    scenarioContext.MessageReceived = true;
+                    return context.Reply(new MyResponse());
+                }
+            }
+        }
+
+        class MyMessage : IMessage
+        {
+        }
+
+        class MyResponse : IMessage
+        {
+        }
+    }
+}
