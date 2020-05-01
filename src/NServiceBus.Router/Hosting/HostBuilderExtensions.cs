49bab19132559afd34486eb5990291a7d86fb18b
Gavazzeni Imerio <imerio.gavazzeni@repower.com>
2020-05-01 20:18:40 +0200

    HostBuilderExtensions

---
+++ src/NServiceBus.Router/Hosting/HostBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace NServiceBus.Router.Hosting
+{
+    /// <summary>
+    /// HostBuilder extensions
+    /// </summary>
+    public static class HostBuilderExtensions
+    {
+        /// <summary>
+        /// Configures the host to start an NServiceBusRouter.
+        /// </summary>
+        public static IHostBuilder UseNServiceBusRouter(this IHostBuilder hostBuilder, Func<HostBuilderContext, RouterConfiguration> routerConfigurationBuilder)
+        {
+            hostBuilder.ConfigureServices((ctx, serviceCollection) =>
+            {
+                var routerConfiguration = routerConfigurationBuilder(ctx);
+
+                serviceCollection.AddSingleton(_ => Router.Create(routerConfiguration));
+                serviceCollection.AddSingleton<IHostedService, NServiceBusRouterHostedService>();
+            });
+
+            return hostBuilder;
+        }
+    }
+}
---
+++ src/NServiceBus.Router/Hosting/NServiceBusRouterHostedService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NServiceBus.Router.Hosting
+{
+    /// <summary>
+    /// IHostedService for NServiceBusRouter
+    /// </summary>
+    public class NServiceBusRouterHostedService : IHostedService
+    {
+        private readonly IRouter _router;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="router"></param>
+        public NServiceBusRouterHostedService(IRouter router)
+        {
+            _router = router;
+        }
+
+        /// <summary>
+        /// StartAsync
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _router.Start().ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// StopAsync
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _router.Stop().ConfigureAwait(false);
+        }
+    }
+}
